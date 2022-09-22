88a4eba5a8ad9b4acd7cb565aa4880b828e98667
Tiffany Rose <tiffany.drose@gmail.com>
2022-09-22 11:25:59 -0400

    first commit

---
+++ src/test/java/com/techtalentsouth/OAuth2Demo/OAuth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.OAuth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
